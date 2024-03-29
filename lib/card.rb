83d2fc0fc53ca66f52a68eb97b4fd6978f8761ce
Connor Priester <cipriester01@gmail.com>
2024-03-28 22:02:18 -0500

    Added comments

--- lib/card.rb
+++ lib/card.rb
@@ -1,12 +1,16 @@
 class Card
   attr_reader :suit, :value
 
-  # local card values variable will use later
-  CARD_VALS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+  # local card values array will use later (had to change var name i used it too much lol)
+  card_array = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
+  # init and assign suit and value to obj
   def initialize(suit, value)
     @suit = suit
     @value = value
   end
-
+  # returns string that has card details
+  def to_s
+    "#{@value} of #{@suit}"
+  end
 end
